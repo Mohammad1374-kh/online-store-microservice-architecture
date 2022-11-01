0e964054acd3ff61f20686f7a909f1a8172d20f1
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-11-01 18:14:38 +0330

    post and getAll methods of controller completed, everything's working fine!

--- src/main/java/com/mohammad/productservice/controller/ProductController.java
+++ src/main/java/com/mohammad/productservice/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.mohammad.productservice.controller;
 
 import com.mohammad.productservice.dto.ProductRequest;
+import com.mohammad.productservice.dto.ProductResponse;
 import com.mohammad.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -13,10 +16,20 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
+/*----------------Creating a new product in DB-------------------------------------*/
+
+    @PostMapping("/add-product")
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct (@RequestBody ProductRequest productRequest){
 
         productService.createProduct(productRequest);
+    }
+
+/*----------------Reading all products from data base------------------------------*/
+
+    @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/mohammad/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/mohammad/productservice/service/ProductServiceImpl.java
@@ -1,12 +1,15 @@
 package com.mohammad.productservice.service;
 
 import com.mohammad.productservice.dto.ProductRequest;
+import com.mohammad.productservice.dto.ProductResponse;
 import com.mohammad.productservice.model.Product;
 import com.mohammad.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -25,5 +28,19 @@ public class ProductServiceImpl implements ProductService{
 
         productRepository.save(product);
         log.info("The product {} is saved",product.getId());
+    }
+
+    @Override
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products =productRepository.findAll();
+        return products.stream().map(this:: mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice()).build();
     }
 }
