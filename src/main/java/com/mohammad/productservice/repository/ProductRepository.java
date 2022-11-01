98248a7be0e5096f3af9158bb03ee869683c48ce
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-11-01 22:38:56 +0330

    creating testContainer integration to test post method

--- src/main/java/com/mohammad/productservice/repository/ProductRepository.java
+++ src/main/java/com/mohammad/productservice/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.mohammad.productservice.repository;
 
 import com.mohammad.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@org.springframework.stereotype.Repository
 public interface ProductRepository extends MongoRepository<Product,String> {
 
 }
